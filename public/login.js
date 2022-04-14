5d6c22b6a7ec04a67ddc50d7fec0567189a88e49
bryanhennes <bryanhennes@gmail.com>
2022-04-14 18:01:50 -0400

    added more validation for logging in and registering on login page

--- public/login.js
+++ public/login.js
@@ -1,3 +1,5 @@
+
+
 function validate()
 {
     var username = document.getElementById("username").value;
@@ -18,5 +20,22 @@ function validate()
         return;
     }
 
+
+}
+
+//convert firebase authentication errors to more readable error messages
+function getErrorMessage(errorMessage)
+
+{
+    //create map of error codes with corresponding value as message to be displayed/returned
+    var map = {};
+    map['auth/invalid-email'] = 'Please enter a valid email';
+    map['auth/invalid-password'] = 'Password must be at least 6 characters';
+    map['auth/wrong-password'] = 'Password is incorrect'
+    map['auth/user-not-found'] = "Account with that email does not exist";
+    map['auth/email-already-exists'] = "An account with that email already exists";
+
+
+    return map[errorMessage];
 
 }
