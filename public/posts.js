93e9967f3971a376cf9c6d104d5674828d2843d3
cmiles1 <cmiles287@gmail.com>
2022-04-19 23:43:07 -0400

    Added searchbar IDs and functions for querying petition data

--- public/posts.js
+++ public/posts.js
@@ -28,8 +28,9 @@ const dbRef = ref(getDatabase());
    * @param {string} view Amount of views this post has
    * @param {string} lastReplyDate Date of the last comment, in format MMM DD YYYY
    * @param {string} lastReplyUser Username of the last commenter
+   * @param {string} statusImage Status image for the post
 */
-function addRow(y, user, title, replyAmount, views, lastReplyDate, lastReplyUser) {
+function addRow(y, user, title, replyAmount, views, lastReplyDate, lastReplyUser, statusImg) {
 	// Create row for this entry
 	const tableRow = document.createElement('div');
 	tableRow.className = 'table-row';
@@ -38,8 +39,7 @@ function addRow(y, user, title, replyAmount, views, lastReplyDate, lastReplyUser
 	// Add status image
 	const status = document.createElement('div');
 	status.className = 'status';
-	status.innerHTML = '<i class="fa fa-fire">';
-	
+	status.innerHTML = `<i class="${statusImg}"></i>`;
 	// Add subject title
 	const subjects = document.createElement('div');
 	subjects.className = 'subjects';
@@ -86,17 +86,15 @@ function findLastReply(record) {
 		// Store the last post information
 		var lastPostTime = 0;
 		var lastPost = record.comments.comment1;
-
 		// Loop over each comment
-		for (var key in record.comments){
+		for (var n = 0; n < Object.values(record.comments).length; n++){
 			// Store the time this comment was posted
-			var postTime = Date.parse(key.date);
-
+			var postTime = Date.parse(Object.values(record.comments)[n].date);
 			// If this post is newer than the last post found
 			if (lastPostTime < postTime){
 				// This is the latest post
 				lastPostTime = postTime;
-				lastPost = key;
+				lastPost = Object.values(record.comments)[n];
 			}
 		}
 		return lastPost;
@@ -104,32 +102,128 @@ function findLastReply(record) {
 	return null;
 }
 
+
+/**
+   * Queries data from a single post and its comments
+   * Checks if the query is included in the post's title, text, or comments
+   * @param record {object} petition record in JSON form
+   * @param query {string} [query=""] Query to run
+   * @return {bool} True if post contains query, otherwise false
+**/
+function queryEverything(record, query=""){
+	// If the query is not found in the post title or text
+	if (!record.petitionTitle.includes(query) && 
+		!record.petitionText.includes(query)){
+		
+		// Check if the post has comments
+		if (record.comments && record.comments.comment1){
+			for (var n = 0; n < Object.values(record.comments).length; n++){
+				var author = Object.values(record.comments)[n].author;
+				var commentText = Object.values(record.comments)[n].comment;
+				// Check comment and comment author for queried text
+				if (commentText.includes(query) || 
+					author.includes(query)){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	return true;
+}
+
+/**
+   * Queries data from a post's title only
+   * Checks if the query is included in the post's title
+   * @param record {object} petition record in JSON form
+   * @param query {string} [query=""] Query to run
+   * @return {bool} True if title contains query, otherwise false
+**/
+function queryByTitle(record, query=""){
+	return record.petitionTitle.includes(query) ? true : false;
+}
+
+/**
+   * Queries data from a post's content only
+   * Checks if the query is included in the post's description
+   * @param record {object} petition record in JSON form
+   * @param query {string} [query=""] Query to run
+   * @return {bool} True if description contains query, otherwise false
+**/
+function queryByDescription(record, query=""){
+	return record.petitionText.includes(query) ? true : false;
+}
+
+/**
+   * Fetches status picture from images, given the date of the last reply
+   * @param {Date} [lastReplyDate=0] Date of last reply (in Unix time)
+   * @return {string} filename of status icon image
+**/
+function getStatusImage(lastReplyDate=0){
+	const today = new Date();
+
+	if (lastReplyDate > today.getDate() - 1/4){
+		// In the last 6 hours
+		return "fa fa-rocket";
+	}
+	else if (lastReplyDate > today.getDate() - 1){
+		// Since yesterday
+		return "fa fa-fire";
+	}
+	else if (lastReplyDate > today.getDate() - 7){
+		// In the last week
+		return "fa fa-book";
+	}
+	// Longer than a week
+	return "fa fa-frown-o";
+}
+
 /**
    * Fetches post information from Firebase
    * Calls the AddRow function for each post
-   * @param query {string} [query=""] query to run
+   * @param query {string} [query=""] Query to run
+   * @param queryType {string} [queryType="Everything"] Type of Query to run ("Everything" || "Titles" || "Description")
+   * @param startY {int} [startY=1] Post in query to start at
+   * @param amountOfPosts {int} [amountOfPosts=10] Amount of posts to display
 **/
-function fetchPosts(query=""){
+function fetchPosts(query="", queryType="Everything", startY=1, amountOfPosts=10){
+	// Clear current petitions listed on page
+	document.getElementById('container').innerHTML = "";
+	
 	// Get petitions from database
 	get(child(dbRef, `petitions`)).then((snapshot) => {
 		// Check if the petitions exist
 		if (snapshot.exists()) {
-			// Store each row
-			var y = 1;
-
-			// *******************************************************************
-			// TODO : 1) Replace or edit forEach to loop over posts matching query
-			// 	  2) Only show a certain number of posts per page
-			//	  3) Implement page buttons
-			// *******************************************************************
-
+			// Store row/petition number in query
+			var y = startY;
 			// Loop over each petition in the snapshot
-		 	snapshot.forEach(function (childSnapshot) {
+		 	snapshot.forEach(function (childSnapshot) {	
+				// Only display amountOfPosts posts on this page
+				if (y >= startY + amountOfPosts) { 
+					return;
+				}
+								
 				// Load data from petition
-            			var value = childSnapshot.val();
-            			var user = value.username;
+            	var value = childSnapshot.val();
+				
+
+				// Only display posts that contain the query in selected content
+				if (query != ""){
+					if (queryType == "Everything" && !queryEverything(value, query)){
+						return;
+					}
+					else if (queryType == "Titles" && !queryByTitle(value, query)){
+						return;
+					}
+					else if (queryType == "Description" && !queryByDescription(value, query)){
+						return;
+					}
+				}
+				// Store data about the petition
+            	var user = value.username;
 				var title = value.petitionTitle;
 				var views = value.views;
+				
 
 				// Store data about the latest comment
 				var replyAmount = 0;
@@ -140,13 +234,12 @@ function fetchPosts(query=""){
 				var lastReply = findLastReply(value);
 				if (lastReply) {
 					// Set the latest comment information
-					replyAmount = 0;
-					for (var key in value.comments) {
-						replyAmount++;
-					}
+					replyAmount = Object.values(value.comments).length;
 					lastReplyDate = lastReply.date;
 					lastReplyUser = lastReply.author;
 				}
+				
+				var statusImage = getStatusImage(lastReplyDate);
 
 				// Adds new row of information to posts
 				addRow(y, 
@@ -155,7 +248,8 @@ function fetchPosts(query=""){
 				   replyAmount, 
 				   views, 
 				   lastReplyDate, 
-				   lastReplyUser
+				   lastReplyUser,
+				   statusImage
 				);
 				y++;
 			});
@@ -168,6 +262,32 @@ function fetchPosts(query=""){
 		console.error(error);
 	});
 }
+
+/**
+   * Fetches information from a query
+   * Called when the search button is clicked
+   * Calls the fetchPost function with the info in the search box
+**/
+function fetchPostByQuery(){
+	// Get search button from html
+	var searchButton = document.getElementById('searchBtn');
+
+	// Remove the clickable event listener (prevents button spamming)
+	searchButton.removeEventListener("click", fetchPostByQuery);
+	// Get search query and type of query
+	var search = document.getElementById('searchBar').value;
+	var searchType = document.getElementById('dropdown').value;
+
+	
+	// Fetch new posts, update page
+	fetchPosts(search, searchType);
+	// Allow user to click the button again
+	searchButton.addEventListener("click", fetchPostByQuery);
+}
+
+// Allow user to click magnifying glass to make query
+var searchButton = document.getElementById('searchBtn');
+searchButton.addEventListener("click", fetchPostByQuery);
 
 
 fetchPosts();
